15f5b7e16d248bb1bce685113d3167e1482c24c6
Vishal <kulagodvishal99@gmail.com>
2020-01-31 16:07:37 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Todo-App';
+  todos=[
+
+];
+
+addTodo(newTodoLabel){
+  var newTodo={
+    label:newTodoLabel
+  };
+
+  this.todos.push(newTodo);
+
+}
+deleteTodo(todo){
+  this.todos=this.todos.filter(t=>t.label!=todo.label);
+}
 }
